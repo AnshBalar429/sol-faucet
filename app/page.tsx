d453214acb6eeb496649d1b9d0e9d853af24f1e8
AnshBalar429 <anshbalar2910@gmail.com>
2025-06-23 12:23:35 +0530

    Add solana logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { SolanaLogo } from "./components/SolanaLogo";
 
 export default function HomePage() {
   return (
     <div>
       Initializing Solana Wallet Adapter...
+      <SolanaLogo/>
     </div>
   );
 }
